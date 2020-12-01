10f9be22ff06462b577fbd120266e88d4fb793fc
Mark Messner <mark.messner@gmail.com>
2020-12-01 08:32:13 -0600

    Updated for FY20

--- make.sh
+++ make.sh
@@ -7,5 +7,5 @@ htlatex web
 biber web
 htlatex web
 
-rm -f *.aux *.bbl *.bcf *.blg *.fdb_latexmk *.fls *.log *.out *.run.xml *.4ct *.4tc *.tmp *.xref *.dvi *.idv *.lg
+rm -f *.aux *.bbl *.bcf *.blg *.fdb_latexmk *.fls *.log *.out *.xdv *.run.xml *.4ct *.4tc *.tmp *.xref *.dvi *.idv *.lg
 
